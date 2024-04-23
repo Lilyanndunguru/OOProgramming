082006e0564381737560cf258c56a2bd6b0bd51e
Lilyanndunguru <167504361+Lilyanndunguru@users.noreply.github.com>
2024-04-23 08:47:28 +0300

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conditionalstatement;
+
+/**
+ *
+ * @author LILIAN
+ */
+public class Operation {
+    int number1,number2;
+    
+   
+    void Addition(){
+        int sum;
+        sum=number1+number2;
+        System.out.print("The sum is: "+sum);
+             
+    }
+    
+    void Subtraction(){
+        int sub;
+        sub=number1-number2;
+        System.out.print("The difference is: "+sub);
+             
+    }
+    
+    void Multiplication(){
+        int product;
+        product=number1*number2;
+        System.out.print("The product is: "+product);
+             
+    }
+    
+    void Division(){
+        int div;
+        div=number1/number2;
+        System.out.print("The sum is: "+div);
+             
+    }
+}
